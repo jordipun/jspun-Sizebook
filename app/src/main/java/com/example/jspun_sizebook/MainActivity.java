d2ced5b316a927d21fcfa9b561783fcc282b399a
Jordi Pun <jordipun@yahoo.ca>
2017-02-01 15:06:03 -0700

    New activity and changes

--- app/src/main/java/com/example/jspun_sizebook/MainActivity.java
+++ app/src/main/java/com/example/jspun_sizebook/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.jspun_sizebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
+
+import static com.example.jspun_sizebook.R.id.clear;
+import static com.example.jspun_sizebook.R.id.recordlist;
+import static com.example.jspun_sizebook.R.id.save;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView recordList;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button saveButton= (Button) findViewById(save);
+        Button clearButton= (Button) findViewById(clear);
+        recordlist = (ListView) findViewById(R.id.recordlist);
     }
 
 
